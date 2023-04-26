40b68742367acb899015c6ccf459336669ac0628
auqa23 <102765120+auqa23@users.noreply.github.com>
2023-04-26 15:13:04 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,47 +5,41 @@ import { MdAddModerator } from "react-icons/md";
 import { SiProbot } from "react-icons/si";
 import { TbMessage2Code } from "react-icons/tb";
 const config = {
-  name: "ProBot",
-  //BotName
+  name: "Lightny",
   inviteLink:
     "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
-  //BotInvite Link
   Features: [
     {
       name: "Moderation",
       description:
-        "Moderation is the process of eliminating or lessening extremes.",
+        "Moderation can help your server from people breaking rules, being disrespectful, etc.",
       logo: <MdAddModerator className="h-10 w-10" />,
     },
     {
       name: "99% Uptime",
       description:
-        "Uptime is a measure of system reliability, expressed as the percentage of time a machine, typically a computer.",
+        "Our bot is online 99% of the time!",
       logo: <SiProbot className="h-10 w-10" />,
     },
     {
-      name: "Automation",
+      name: "Automoderation",
       description:
-        "Automation describes a wide range of technologies that reduce  human intervention in processes.",
+        "Automoderation protects you server from people spamming messages, ping, etc.",
       logo: <TbMessage2Code className="h-10 w-10" />,
     },
     {
       name: "Welcomer",
       description:
-        "Welcomer is the person who greet the new person in an unique and great way",
+        "Welcomer is the person who greet the new person in an unique and great way.",
       logo: <CgCrown className="h-10 w-10" />,
     },
     // You can added more features if need add
   ],
   //Bot Features
-  serverCount: 300, //-  - Your
-  memberCount: 700, //--   - Bot
-  executedCommand: 27000, //--  - Stats
-  availableCommand: 400, //---   - Here
-  //Do not change if you don't know about them
-  faceBookLogo: (
-    <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
-  ),
+  serverCount: 12, //-  - Your
+  memberCount: 388, //--   - Bot
+  executedCommand: 1085, //--  - Stats
+  availableCommand: 44, //---   - Here
   discordLogo: (
     <FaDiscord className="h-10 w-10 text-[#738adb] hover:scale-125 easeIn cursor-pointer" />
   ),
@@ -54,12 +48,8 @@ const config = {
   ),
   //----------------------------------------------------------------
   // Your social handle links
-  githubLink: "https://github.com/mohitmimani",
-  twitterLink: "https://twitter.com",
-  mediumLink: "https://medium.com/",
-  facebookLink: "https://facebook.com",
-  discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+  discordLink: "https://discord.gg/K349BbTvVm",
+  youtubeLink: "https://youtube.com/@silvxrcoding",
 };
 
 export default config;
